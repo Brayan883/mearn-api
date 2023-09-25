872af094e7166afc2c20a7b8603d232b42b84751
Brayan Rivera Gomez <100776460+Brayan883@users.noreply.github.com>
2023-09-24 21:08:33 -0500

    cambios

--- src/componets/PageProtected.jsx
+++ src/componets/PageProtected.jsx
@@ -1,19 +1,16 @@
 /* eslint-disable react/prop-types */
-import { useEffect, useState } from "react";
+import { useEffect} from "react";
 import { Outlet, Navigate } from "react-router-dom";
 
 const PageProtected = ({ children }) => {
-  const IsAuth = window.localStorage.getItem("IsUser") || false;
-  const [timeRemaining, setTimeRemaining] = useState(0);
+  const IsAuth = window.localStorage.getItem("IsUser") || false;  
   useEffect(() => {
     const expiresIn = 86400000;
     const startTime = window.localStorage.getItem("StartTime") || null;
-    if (IsAuth && startTime) {      
-      const currentTime = new Date().getTime();
-      const elapsedTime = currentTime - parseInt(startTime, 10);
+    if (IsAuth && startTime) {            
+      const elapsedTime = new Date().getTime() - parseInt(startTime, 10);
       if (elapsedTime < expiresIn) {                        
-        const remainingTime = expiresIn - elapsedTime;
-        setTimeRemaining(remainingTime);
+        const remainingTime = expiresIn - elapsedTime;        
         const timeoutId = setTimeout(() => {          
           window.localStorage.removeItem("IsUser");
           window.localStorage.removeItem("StartTime");
@@ -27,10 +24,7 @@ const PageProtected = ({ children }) => {
   }, [IsAuth]);
   if (!IsAuth) return <Navigate to="/" />;
   return (
-    <>
-      {timeRemaining > 0 ? (
-        <div className="text-2xl p-2 text-emerald-600 font-bold text-center"  > Tiempo restante: {Math.floor(timeRemaining / 3600000)} horas</div>
-      ) : null}
+    <>      
       {children ? children : <Outlet />}
     </>
   );
